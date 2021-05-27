37300296e443ef16cbb925d72e653796c913c730
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-05-27 18:06:42 +0400

    lombok anabledt

--- src/main/java/com/example/model/DbData.java
+++ src/main/java/com/example/model/DbData.java
@@ -4,46 +4,17 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "Db_Data")
 public class DbData {
 
     @Id
+    @GeneratedValue
     @Column(name = "data_id")
     private Long dataId;
 
     @Column(name = "dataset")
     private String data;
-
-    public DbData() {
-    }
-
-    public DbData(long dataId) {
-        this.dataId = dataId;
-    }
-
-    public void setDataId(Long dataId) {
-        this.dataId = dataId;
-    }
-
-    public Long getDataId() {
-        return dataId;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return "DbData{" +
-                "dataId=" + dataId +
-                ", data='" + data + '\'' +
-                '}';
-    }
 
 }
