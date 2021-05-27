fc037acd7ad6125c60cdefc8d0c63fb3affc1df8
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-05-27 18:03:54 +0400

    db h2 works

--- src/main/java/com/example/model/DbData.java
+++ src/main/java/com/example/model/DbData.java
@@ -2,17 +2,48 @@ package com.example.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Data
 @Entity
+@Table(name = "Db_Data")
 public class DbData {
 
     @Id
-    @GeneratedValue
+    @Column(name = "data_id")
     private Long dataId;
+
+    @Column(name = "dataset")
     private String data;
+
+    public DbData() {
+    }
+
+    public DbData(long dataId) {
+        this.dataId = dataId;
+    }
+
+    public void setDataId(Long dataId) {
+        this.dataId = dataId;
+    }
+
+    public Long getDataId() {
+        return dataId;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "DbData{" +
+                "dataId=" + dataId +
+                ", data='" + data + '\'' +
+                '}';
+    }
 
 }
