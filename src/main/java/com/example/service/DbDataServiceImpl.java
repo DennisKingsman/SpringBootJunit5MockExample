aa9ee071a01ca055540370644b71f574bb2cc689
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-05-28 21:46:16 +0400

    test2 added

--- src/main/java/com/example/service/DbDataServiceImpl.java
+++ src/main/java/com/example/service/DbDataServiceImpl.java
@@ -2,9 +2,13 @@ package com.example.service;
 
 import com.example.model.DbData;
 import com.example.repository.DbDataRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
+@Slf4j
 @Service
 public class DbDataServiceImpl implements DbDataService {
 
@@ -15,8 +19,16 @@ public class DbDataServiceImpl implements DbDataService {
         this.dbDataRepository = dbDataRepository;
     }
 
-    public DbData findById(Long id) {
-        return dbDataRepository.findById(id).get();
+    public DbData findById(Long id) throws NoSuchElementException {
+        DbData dbData;
+        try {
+            dbData = dbDataRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            log.debug(e.getMessage());
+            log.info(e.getMessage());
+            dbData = null;
+        }
+        return dbData;
     }
 
 }
